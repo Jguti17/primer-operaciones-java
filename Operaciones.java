3405fa184b8e8aaf0d34710beefa9c0f8f588754
jguti17 <jgutierrez21cg@gmail.com>
2022-08-12 10:52:33 -0400

    Operaciones

---
+++ Operaciones.java
@@ -0,0 +1,31 @@
+package operaciones;
+/**
+ *
+ * @author JULIO GUTIERREZ
+ */
+public class Operaciones {
+    public static void main(String[] args) {
+        System.out.println(suma(4, 5));
+    }
+    
+    //suma
+    public static int suma(int a, int b){
+        return a + b;
+    }
+    
+    //resta
+    public static int resta(int a, int b){
+        return a - b;
+    }
+    
+    //multiplicacion
+    public static int multiplicacion(int a, int b){
+        return a * b;
+    }
+    
+    //Division
+    public static int Division(int a, int b){
+        return a / b;
+    }
+    
+}
